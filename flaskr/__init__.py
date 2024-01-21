9fcbce72e3e58a42037caed5288f843301e413d9
phienna <133808318+phienna@users.noreply.github.com>
2024-01-21 13:31:44 +0100

    added cputemp function for raspi

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
-
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    from flaskr.view import index
+    from flaskr.view import index, viewcputemp
+
     app.register_blueprint(index.bp)
+    app.register_blueprint(viewcputemp.bp)
 
     return app
--- flaskr/service/cputemp.py
+++ flaskr/service/cputemp.py
@@ -1,8 +1,10 @@
-def get_cpu_temp():
-    import os
+class Hardware:
 
-    temp = os.popen("vcgencmd measure_temp").readline()
-    temp = (temp)
-    return temp
+    def get_cpu_temp(self):
+        import os
 
-print(get_cpu_temp())
+        temp = os.popen("vcgencmd measure_temp").readline()
+        temp = (temp)
+        return temp 
+
+    #print(get_cpu_temp())
