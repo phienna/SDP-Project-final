51da91d5a0c0a7f203b2b0ef1d958ddf106b366f
phienna <133808318+phienna@users.noreply.github.com>
2024-01-28 18:30:21 +0100

    added config-files

--- flaskr/service/hardware.py
+++ flaskr/service/hardware.py
@@ -5,5 +5,11 @@ class Hardware:
 
         temp = os.popen("vcgencmd measure_temp").readline()
         return temp
+    
+    def get_config_int(self):
+        import os
+
+        config_int = os.popen("vcgencmd get_config int").readline()
+        return config_int
 
 # print(get_cpu_temp())
