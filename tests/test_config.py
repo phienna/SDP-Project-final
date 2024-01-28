0820baa5ed558be3b2b94a9a4daa0ec99496f527
phienna <133808318+phienna@users.noreply.github.com>
2024-01-28 19:03:38 +0100

    finalized config

---
+++ tests/test_config.py
@@ -0,0 +1,21 @@
+import pytest
+
+from flaskr.view.config import config_int
+
+
+@pytest.mark.unit
+def test_config_int(mocker):
+    mocker.patch('flaskr.view.config.Hardware.get_config_int',
+                 return_value=42)
+    config_int = config_int()
+
+    assert '42' == config_int
+
+
+@pytest.mark.api
+def test_config_int_api(mocker, client):
+    mocker.patch('flaskr.view.config.Hardware.get_config_int',
+                 return_value=42)
+    response = client.get('/config/int')
+
+    assert b'42' == response.data
